cfc9a8dd0b17dc46cb6ed9462095b14990b95641
Mitra Mejia <mitra.mejia@gmail.com>
2024-11-18 09:59:24 -0400

    Add home-manager configurations for Linux and macOS
    Introduced comprehensive home-manager configurations for both Linux and macOS. The Linux setup includes extensive settings for Zsh, Vim, Tmux, and other essential tools, while the macOS configuration integrates Homebrew, declarative dock setup, and additional user-specific settings.

---
+++ modules/darwin/home-manager.nix
@@ -0,0 +1,94 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  user = "mitramejia";
+in {
+  imports = [
+    ./dock
+  ];
+
+  # It me
+  users.users.${user} = {
+    name = "${user}";
+    home = "/Users/${user}";
+    isHidden = false;
+    shell = pkgs.zsh;
+  };
+
+  homebrew = {
+    enable = true;
+    casks = pkgs.callPackage ./casks.nix {};
+    # onActivation.cleanup = "uninstall";
+
+    # These app IDs are from using the mas CLI app
+    # mas = mac app store
+    # https://github.com/mas-cli/mas
+    #
+    # $ nix shell nixpkgs#mas
+    # $ mas search <app name>
+    #
+    # If you have previously added these apps to your Mac App Store profile (but not installed them on this system),
+    # you may receive an error message "Redownload Unavailable with This Apple ID".
+    # This message is safe to ignore. (https://github.com/dustinlyons/nixos-config/issues/83)
+    # masApps = {
+    #   "1password" = 1333542190;
+    # };
+  };
+
+  # Enable home-manager
+  home-manager = {
+    useGlobalPkgs = true;
+    users.${user} = {
+      pkgs,
+      config,
+      lib,
+      ...
+    }: {
+      home = {
+        enableNixpkgsReleaseCheck = false;
+        packages = pkgs.callPackage ./packages.nix {};
+        stateVersion = "23.11";
+      };
+      programs = {} // import ../shared/home-manager.nix {inherit config pkgs lib;};
+    };
+    backupFileExtension = "backup";
+  };
+
+  # Fully declarative dock using the latest from Nix Store
+  local.dock.enable = true;
+  local.dock.entries = [
+    {path = "/Applications/Slack.app/";}
+    {path = "/Applications/1Password.app/";}
+    {path = "/Applications/Messages.app/";}
+    {path = "/Applications/Facetime.app/";}
+    {path = "/Applications/Iterm.app/";}
+    {path = "/Applications/Notes.app/";}
+    {path = "/Applications/WhatsApp.app/";}
+    {path = "/Applications/Obsidian.app/";}
+    {path = "/Users/mitramejia/Applications/WebStorm.app";}
+    {path = "/Users/mitramejia/Applications/DataGrip.app";}
+    {path = "/Users/mitramejia/Applications/Android\ Studio.app/";}
+    {path = "/Applications/Music.app/";}
+    {path = "/Applications/Photos.app/";}
+    {path = "/Applications/Safari.app/";}
+    {path = "/Applications/Cursor.app/";}
+    {path = "/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/";}
+    {
+      path = "/Applications";
+      section = "others";
+      options = "--sort name --view grid --display folder";
+    }
+    {
+      path = "${config.users.users.${user}.home}/Downloads";
+      section = "others";
+      options = "--sort name --view grid --display folder";
+    }
+    {
+      path = "${config.users.users.${user}.home}/Desktop";
+      section = "others";
+      options = "--sort name --view grid --display folder";
+    }
+  ];
+}
---
+++ modules/shared/home-manager.nix
@@ -0,0 +1,385 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  name = "Mitra Mejia	";
+  user = "mitra";
+  email = "mitra.mejia@gmail.com";
+in {
+  direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
+
+  zsh = {
+    enable = true;
+    autosuggestion = {
+      enable = true;
+    };
+    autocd = true;
+    cdpath = ["~/.local/share/src"];
+    initExtraFirst = ''
+      if [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
+        . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+        . /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+      fi
+
+      if [[ "$(uname)" == "Linux" ]]; then
+        alias pbcopy='xclip -selection clipboard'
+      fi
+
+      # Define variables for directories
+      export PATH=$HOME/.pnpm-packages/bin:$HOME/.pnpm-packages:$PATH
+      export PATH=$HOME/.npm-packages/bin:$HOME/bin:$PATH
+      export PATH=$HOME/.composer/vendor/bin:$PATH
+      export PATH=$HOME/.local/share/bin:$PATH
+
+      export ANDROID_HOME=$HOME/Library/Android/sdk
+
+      export PNPM_HOME=~/.pnpm-packages
+      alias p=pnpm
+      alias px=pnpx
+
+      # Remove history data we don't want to see
+      export HISTIGNORE="pwd:ls:cd"
+
+      # Ripgrep alias
+      alias search='rg -p --glob "!node_modules/*" --glob "!vendor/*" "$@"'
+
+      # Vim is my editor
+      export ALTERNATE_EDITOR=""
+      export EDITOR="vim"
+
+      alias watch="tmux new-session -d -s watch-session 'zsh ./bin/watch.sh'"
+      alias unwatch='tmux kill-session -t watch-session'
+
+      # Use difftastic, syntax-aware diffing
+      alias diff=difft
+
+      alias gp='git push'
+      alias gplo='git pull origin'
+
+      # Always color ls and group directories
+      alias ls='ls --color=auto'
+
+    '';
+    initExtra = ''
+      [ -s "/Users/mitramejia/.scm_breeze/scm_breeze.sh" ] && source "/Users/mitramejia/.scm_breeze/scm_breeze.sh"
+    '';
+    oh-my-zsh = {
+      enable = true;
+      plugins = ["git" "aws" "z" "nvm" "macos" "node" "vi-mode"];
+      theme = "";
+    };
+  };
+
+  starship = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  git = {
+    enable = true;
+    ignores = ["*.swp"];
+    userName = name;
+    userEmail = email;
+    lfs = {
+      enable = true;
+    };
+    extraConfig = {
+      init.defaultBranch = "main";
+      core = {
+        editor = "vim";
+        autocrlf = "input";
+      };
+      commit.gpgsign = true;
+      pull.rebase = false;
+    };
+  };
+
+  vim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [vim-airline vim-airline-themes copilot-vim vim-startify vim-tmux-navigator];
+    settings = {ignorecase = true;};
+    extraConfig = ''
+      "" General
+      set number
+      set history=1000
+      set nocompatible
+      set modelines=0
+      set encoding=utf-8
+      set scrolloff=3
+      set showmode
+      set showcmd
+      set hidden
+      set wildmenu
+      set wildmode=list:longest
+      set cursorline
+      set ttyfast
+      set nowrap
+      set ruler
+      set backspace=indent,eol,start
+      set laststatus=2
+      set clipboard=autoselect
+
+      " Dir stuff
+      set nobackup
+      set nowritebackup
+      set noswapfile
+      set backupdir=~/.config/vim/backups
+      set directory=~/.config/vim/swap
+
+      " Relative line numbers for easy movement
+      set relativenumber
+      set rnu
+
+      "" Whitespace rules
+      set tabstop=8
+      set shiftwidth=2
+      set softtabstop=2
+      set expandtab
+
+      "" Searching
+      set incsearch
+      set gdefault
+
+      "" Statusbar
+      set nocompatible " Disable vi-compatibility
+      set laststatus=2 " Always show the statusline
+      let g:airline_theme='bubblegum'
+      let g:airline_powerline_fonts = 1
+
+      "" Local keys and such
+      let mapleader=","
+      let maplocalleader=" "
+
+      "" Change cursor on mode
+      :autocmd InsertEnter * set cul
+      :autocmd InsertLeave * set nocul
+
+      "" File-type highlighting and configuration
+      syntax on
+      filetype on
+      filetype plugin on
+      filetype indent on
+
+      "" Paste from clipboard
+      nnoremap <Leader>, "+gP
+
+      "" Copy from clipboard
+      xnoremap <Leader>. "+y
+
+      "" Move cursor by display lines when wrapping
+      nnoremap j gj
+      nnoremap k gk
+
+      "" Map leader-q to quit out of window
+      nnoremap <leader>q :q<cr>
+
+      "" Move around split
+      nnoremap <C-h> <C-w>h
+      nnoremap <C-j> <C-w>j
+      nnoremap <C-k> <C-w>k
+      nnoremap <C-l> <C-w>l
+
+      "" Easier to yank entire line
+      nnoremap Y y$
+
+      "" Move buffers
+      nnoremap <tab> :bnext<cr>
+      nnoremap <S-tab> :bprev<cr>
+
+      "" Like a boss, sudo AFTER opening the file to write
+      cmap w!! w !sudo tee % >/dev/null
+
+      let g:startify_lists = [
+        \ { 'type': 'dir',       'header': ['   Current Directory '. getcwd()] },
+        \ { 'type': 'sessions',  'header': ['   Sessions']       },
+        \ { 'type': 'bookmarks', 'header': ['   Bookmarks']      }
+        \ ]
+
+      let g:startify_bookmarks = [
+        \ '~/.local/share/src',
+        \ ]
+
+      let g:airline_theme='bubblegum'
+      let g:airline_powerline_fonts = 1
+    '';
+  };
+
+  alacritty = {
+    enable = true;
+    settings = {
+      cursor = {
+        style = "Block";
+      };
+
+      window = {
+        opacity = 1.0;
+        padding = {
+          x = 24;
+          y = 24;
+        };
+      };
+
+      font = {
+        normal = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Regular";
+        };
+        size = lib.mkMerge [
+          (lib.mkIf pkgs.stdenv.hostPlatform.isLinux 10)
+          (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin 14)
+        ];
+      };
+
+      colors = {
+        primary = {
+          background = "0x1f2528";
+          foreground = "0xc0c5ce";
+        };
+
+        normal = {
+          black = "0x1f2528";
+          red = "0xec5f67";
+          green = "0x99c794";
+          yellow = "0xfac863";
+          blue = "0x6699cc";
+          magenta = "0xc594c5";
+          cyan = "0x5fb3b3";
+          white = "0xc0c5ce";
+        };
+
+        bright = {
+          black = "0x65737e";
+          red = "0xec5f67";
+          green = "0x99c794";
+          yellow = "0xfac863";
+          blue = "0x6699cc";
+          magenta = "0xc594c5";
+          cyan = "0x5fb3b3";
+          white = "0xd8dee9";
+        };
+      };
+    };
+  };
+
+  ssh = {
+    enable = true;
+    includes = [
+      (
+        lib.mkIf pkgs.stdenv.hostPlatform.isLinux
+        "/home/${user}/.ssh/config_external"
+      )
+      (
+        lib.mkIf pkgs.stdenv.hostPlatform.isDarwin
+        "/Users/${user}/.ssh/config_external"
+      )
+    ];
+    matchBlocks = {
+      "github.com" = {
+        identitiesOnly = true;
+        identityFile = [
+          (
+            lib.mkIf pkgs.stdenv.hostPlatform.isLinux
+            "/home/${user}/.ssh/id_github"
+          )
+          (
+            lib.mkIf pkgs.stdenv.hostPlatform.isDarwin
+            "/Users/${user}/.ssh/id_github"
+          )
+        ];
+      };
+    };
+  };
+
+  tmux = {
+    enable = true;
+    shell = "${pkgs.zsh}/bin/zsh";
+    plugins = with pkgs.tmuxPlugins; [
+      vim-tmux-navigator
+      sensible
+      yank
+      prefix-highlight
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavour 'mocha'
+          set -g @catppuccin_window_tabs_enabled on
+          set -g @catppuccin_date_time "%H:%M"
+        '';
+      }
+      {
+        plugin = resurrect; # Used by tmux-continuum
+
+        # Use XDG data directory
+        # https://github.com/tmux-plugins/tmux-resurrect/issues/348
+        extraConfig = ''
+          set -g @resurrect-dir '/Users/dustin/.cache/tmux/resurrect'
+          set -g @resurrect-capture-pane-contents 'on'
+          set -g @resurrect-pane-contents-area 'visible'
+        '';
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '5' # minutes
+        '';
+      }
+    ];
+    terminal = "screen-256color";
+    prefix = "C-Space";
+    escapeTime = 10;
+    historyLimit = 50000;
+    extraConfig = ''
+      # Remove Vim mode delays
+      set -g focus-events on
+
+      # Enable full mouse support
+      set -g mouse on
+
+      # -----------------------------------------------------------------------------
+      # Key bindings
+      # -----------------------------------------------------------------------------
+
+      # Unbind default keys
+      unbind C-b
+      unbind '"'
+      unbind %
+
+      # Split panes, vertical or horizontal
+      bind-key / split-window -h
+      bind-key - split-window -v
+
+      # Move around panes with vim-like bindings (h,j,k,l)
+      bind-key -n M-k select-pane -U
+      bind-key -n M-h select-pane -L
+      bind-key -n M-j select-pane -D
+      bind-key -n M-l select-pane -R
+
+      # Smart pane switching with awareness of Vim splits.
+      # This is copy paste from https://github.com/christoomey/vim-tmux-navigator
+      is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+        | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|n?vim?x?)(diff)?$'"
+      bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
+      bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
+      bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
+      bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
+      tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
+      if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
+        "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
+      if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
+        "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
+
+      bind-key -T copy-mode-vi 'C-h' select-pane -L
+      bind-key -T copy-mode-vi 'C-j' select-pane -D
+      bind-key -T copy-mode-vi 'C-k' select-pane -U
+      bind-key -T copy-mode-vi 'C-l' select-pane -R
+      bind-key -T copy-mode-vi 'C-\' select-pane -l
+    '';
+  };
+}
