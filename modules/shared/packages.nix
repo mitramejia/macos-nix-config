1f1017f00db460ec717ee9c6b730e392b2c73e99
Mitra Mejia <mitra.mejia@gmail.com>
2024-11-18 09:59:28 -0400

    Add home-manager configurations for Linux and macOS
    Introduced comprehensive home-manager configurations for both Linux and macOS. The Linux setup includes extensive settings for Zsh, Vim, Tmux, and other essential tools, while the macOS configuration integrates Homebrew, declarative dock setup, and additional user-specific settings.

---
+++ modules/shared/packages.nix
@@ -0,0 +1,86 @@
+{pkgs, ...}:
+with pkgs; [
+  devenv
+
+  # General packages for development and system management
+  act
+  aspell
+  aspellDicts.en
+  bash-completion
+  bat
+  btop
+  coreutils
+  difftastic
+  du-dust
+  gcc
+  git-filter-repo
+  killall
+  neofetch
+  openssh
+  pandoc
+  sqlite
+  ruby
+  pnpm
+  wget
+  zip
+  nil
+  neovim
+  lunarvim
+  alejandra
+  android-tools
+  openjdk
+
+  # Encryption and security tools
+  _1password-cli
+  age
+  gnupg
+  libfido2
+  sops
+
+  # Cloud-related tools and SDKs
+  docker
+  docker-compose
+  flyctl
+
+  go
+  gopls
+  ngrok
+  ssm-session-manager-plugin
+  terraform
+  terraform-ls
+  tflint
+
+  # Media-related packages
+  imagemagick
+  ffmpeg
+  fd
+  font-awesome
+  glow
+  jpegoptim
+  pngquant
+
+  # Node.js development tools
+  fzf
+  nodePackages.live-server
+  nodePackages.nodemon
+  nodePackages.eas-cli
+
+  # Source code management, Git, GitHub tools
+  gh
+
+  # Text and terminal utilities
+  htop
+  hunspell
+  iftop
+  jq
+  ripgrep
+  tree
+  tmux
+  unrar
+  unzip
+
+  # Python packages
+  black
+  python39
+  python39Packages.virtualenv
+]
