3a5fa2fe74af8d0f1624c53150ceeb0006f56bad
mitramejia <mitra.mejia@gmail.com>
2025-09-11 09:22:02 -0500

    Update flake.nix to use stable 25.05 release versions for nixpkgs, home-manager, and nix-darwin inputs

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "Starter Configuration for MacOS";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+      home-manager.url = "github:nix-community/home-manager/release-25.05";
     darwin = {
-      url = "github:LnL7/nix-darwin/master";
+      url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-homebrew = {
